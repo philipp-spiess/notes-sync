4c63506c73e460affa82a4613d9c1c5a382ddc6a
Philipp Spiess <hello@philippspiess.com>
2022-02-21 20:21:56 +0100

    commit

--- sync.js
+++ sync.js
@@ -1,6 +1,10 @@
 const chokidar = require("chokidar");
 
+const WATCH_DIR = process.env.WATCH_DIR;
+
+console.log("Starting watching on: ", WATCH_DIR);
+
 // One-liner for current directory
-chokidar.watch(process.env.WATCH_DIR).on("all", (event, path) => {
+chokidar.watch(WATCH_DIR).on("all", (event, path) => {
   console.log(event, path);
 });
